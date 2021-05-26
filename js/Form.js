d779e55d54dd845c77af14e67b8484c3dc9c8fb4
fareesa_mas <30589399+fareesa1@users.noreply.github.com>
2021-05-26 20:09:11 +0530

    Update Form.js

--- js/Form.js
+++ js/Form.js
@@ -22,11 +22,11 @@ class Form {
     this.button.mousePressed(()=>{
       this.input.hide();
       this.button.hide();
-      player.name = this.input.value();
-      playerCount+=1;
-      player.index = playerCount;
-     // player.update();
-      player.updateCount(playerCount);
+//       player.name = this.input.value();
+//       playerCount+=1;
+//       player.index = playerCount;
+//      // player.update();
+//       player.updateCount(playerCount);
       this.greeting.html("Hello " + player.name)
       this.greeting.position(130, 100);
     });
